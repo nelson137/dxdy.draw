c3d70523cbfa7df7f9b80a49d161c82f6ab5d471
Nelson Earle <nelson.earle137@gmail.com>
2025-04-20 12:20:13 -0500

    WIP

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 // when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::sync::atomic::*;
+
 use anyhow::Result;
 
 slint::slint! {
@@ -10,10 +12,21 @@ slint::slint! {
 
         out property <length> canvas-width <=> canvas.width;
         out property <length> canvas-height <=> canvas.height;
+        out property <bool> canvas-has-hover <=> touch-area.has-hover;
 
-        canvas := Image {
-            width: 800px;
-            height: 600px;
+        callback render(duration, length, length);
+        public function tick() {
+            render(animation-tick(), touch-area.mouse-x, touch-area.mouse-y);
+        }
+
+        touch-area := TouchArea {
+            changed mouse-x => { root.tick(); }
+            changed mouse-y => { root.tick(); }
+
+            canvas := Image {
+                width: 800px;
+                height: 600px;
+            }
         }
     }
 }
@@ -22,6 +35,7 @@ type Pixel = slint::Rgb8Pixel;
 
 mod colors {
     use super::Pixel;
+    pub(crate) const BLACK: Pixel = Pixel::new(0x00, 0x00, 0x00);
     pub(crate) const BLUE: Pixel = Pixel::new(0x60, 0x60, 0xff);
     pub(crate) const RED: Pixel = Pixel::new(0xff, 0x60, 0x60);
 }
@@ -54,21 +68,32 @@ impl UiExts for Ui {
 }
 
 trait CanvasBufferExts {
+    fn coords(&self, x: isize, y: isize) -> (isize, isize);
     fn iter_pixels(
         &mut self,
-        width: usize,
-        f: impl FnMut(&mut Pixel, (usize, usize)),
+        width: isize,
+        f: impl FnMut(&mut Pixel, (isize, isize)),
     );
 }
 
 impl CanvasBufferExts for CanvasBuffer {
+    fn coords(&self, x: isize, y: isize) -> (isize, isize) {
+        let x = x - self.width() as isize / 2;
+        let y = self.height() as isize / 2 - y;
+        (x, y)
+    }
+
     fn iter_pixels(
         &mut self,
-        width: usize,
-        mut f: impl FnMut(&mut Pixel, (usize, usize)),
+        width: isize,
+        mut f: impl FnMut(&mut Pixel, (isize, isize)),
     ) {
+        let half_width = self.width() as isize / 2;
+        let half_height = self.height() as isize / 2;
         for (i, pixel) in self.make_mut_slice().iter_mut().enumerate() {
-            f(pixel, (i % width, i / width));
+            let x = (i as isize % width) - half_width;
+            let y = half_height - (i as isize / width);
+            f(pixel, (x, y));
         }
     }
 }
@@ -90,29 +115,50 @@ impl SetTimeout {
 fn main() -> Result<()> {
     let ui = Ui::new()?;
 
+    static DOT_COLOR: AtomicBool = AtomicBool::new(true);
+
     SetTimeout::from_millis(750).run({
         let ui = ui.as_weak();
         move || {
-            ui.upgrade().unwrap().update_canvas(|buffer| {
-                use std::sync::atomic::*;
+            DOT_COLOR.fetch_xor(true, Ordering::Relaxed);
+            let ui = ui.upgrade().unwrap();
+            ui.invoke_tick();
+        }
+    });
 
-                static X: AtomicBool = AtomicBool::new(true);
+    ui.on_render({
+        let ui = ui.as_weak();
+        move |t, mouse_x, mouse_y| {
+            static LAST_T: AtomicI64 = AtomicI64::new(0);
+            let last_t = LAST_T.load(Ordering::Relaxed);
+            if t <= last_t || t - last_t < 24 {
+                return;
+            }
+            LAST_T.store(t, Ordering::Relaxed);
 
-                let x = X.fetch_xor(true, Ordering::Relaxed);
-                let (color_bg, color_dot) = if x {
-                    (colors::BLUE, colors::RED)
+            let ui = ui.upgrade().unwrap();
+
+            let hover = ui.get_canvas_has_hover();
+
+            ui.update_canvas(|buffer| {
+                let (mouse_x, mouse_y) =
+                    buffer.coords(mouse_x as isize, mouse_y as isize);
+
+                let color_dot = if DOT_COLOR.load(Ordering::Relaxed) {
+                    colors::BLUE
                 } else {
-                    (colors::RED, colors::BLUE)
+                    colors::RED
                 };
 
-                let width = buffer.width() as usize;
-                let height = buffer.height() as usize;
-                buffer.iter_pixels(width, move |pixel, (x, y)| {
-                    let x = x as isize - width as isize / 2;
-                    let y = y as isize - height as isize / 2;
-                    let dist2 = x * x + y * y;
-                    const R: isize = 64;
-                    *pixel = if dist2 < R * R { color_dot } else { color_bg };
+                let width = buffer.width() as isize;
+                buffer.iter_pixels(width, |pixel, (x, y)| {
+                    let (x, y) = (x - mouse_x, y - mouse_y);
+                    const R: isize = 32;
+                    *pixel = if hover && x * x + y * y < R * R {
+                        color_dot
+                    } else {
+                        colors::BLACK
+                    };
                 });
             });
         }
